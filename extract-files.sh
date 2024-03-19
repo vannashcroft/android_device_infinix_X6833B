8f0e7848c00c90dcca1d6a9d1d43b79363873f49
muhammadrafiasyddiq <muhammadrafigaming8@gmail.com>
2024-03-19 14:13:00 +0000

    X6833B: Link media.c2 service against v33 `libstagefright_foundation`
     * Fixes crashes and high CPU usage

--- extract-files.sh
+++ extract-files.sh
@@ -77,6 +77,9 @@ function blob_fixup {
             "${PATCHELF}" --replace-needed "android.hardware.security.sharedsecret-V1-ndk_platform.so" "android.hardware.security.sharedsecret-V1-ndk.so" "${2}"
             "${PATCHELF}" --add-needed "android.hardware.security.rkp-V1-ndk.so" "${2}"
             ;;
+            vendor/bin/hw/android.hardware.media.c2@1.2-mediatek)
+            "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
+            ;;
     esac
 }
 
