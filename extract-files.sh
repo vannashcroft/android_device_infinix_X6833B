1c4a0d62420155d6b6e40ae6b0da93ee173103f5
muhammadrafiasyddiq <muhammadrafigaming8@gmail.com>
2024-03-26 10:19:42 +0000

    X6833B: Import Camera Stack

--- extract-files.sh
+++ extract-files.sh
@@ -80,6 +80,11 @@ function blob_fixup {
             vendor/bin/hw/android.hardware.media.c2@1.2-mediatek)
             "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
             ;;
+            vendor/bin/hw/mt6789/camerahalserver)
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v31.so" "${2}"
+            "${PATCHELF}" --replace-needed "libbinder.so" "libbinder-v31.so" "${2}"
+            "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v31.so" "${2}"
+            ;;
     esac
 }
 
