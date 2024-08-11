27d39bd43ae8efad91abf9cc248124292998d851
bengris32 <bengris32@protonmail.ch>
2024-08-11 21:03:16 +0530

    X6833B: Link main Audio HW module against V33 libstagefright_foundation
    Change-Id: Id2dfd90a671846fb0108503d642195eaa4a25f12
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: muhammadrafiasyddiq <muhammadrafigaming8@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -94,6 +94,7 @@ function blob_fixup {
             vendor/lib*/hw/audio.primary.mediatek.so)
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
+            "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
             ;;
     esac
 }
