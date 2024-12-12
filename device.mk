bc422e06d66cf66999c7d0fa5079170bec781391
Alcatraz323 <alcatraz32323@gmail.com>
2024-12-12 21:07:07 +0800

    X6833B: Implement and Build DSPVolumeSynchronizer
    Some Transsion devices have a speaker that needs a framework to cooperate
    with DSP to synchronize volume so that the DSP can limit bass when the
    volume is high to prevent distortion.
    
    Change-Id: I750803d94161e1e7482552d2a39566f42e82fc0a

--- device.mk
+++ device.mk
@@ -64,7 +64,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
-    android.hardware.audio.service
+    android.hardware.audio.service \
+    DSPVolumeSynchronizer
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
