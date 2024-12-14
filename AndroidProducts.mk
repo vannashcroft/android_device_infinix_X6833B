c43004c2f53a319beeb9507a00ccf2f4e0482c15
muhammadrafiasyddiq <muhammadrafigaming8@gmail.com>
2024-12-14 11:38:13 +0700

    X6833B: Clean Up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,19 +16,11 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X6833B.mk \
-    $(LOCAL_DIR)/superior_X6833B.mk \
-    $(LOCAL_DIR)/evolution_X6833B.mk
+    $(LOCAL_DIR)/lineage_X6833B.mk
    
 
 COMMON_LUNCH_CHOICES := \
     lineage_X6833B-user \
     lineage_X6833B-userdebug \
-    lineage_X6833B-eng \
-    superior_X6833B-user \
-    superior_X6833B-userdebug \
-    superior_X6833B-eng \
-    evolution_X6833B-user \
-    evolution_X6833B-userdebug \
-    evolution_X6833B-eng 
+    lineage_X6833B-eng
     
